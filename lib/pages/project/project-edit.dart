f4ef73bcf41f1735c41c5e496e0f90c8bd9a6fea
Antonio Higino B L Medeiros <antoniobisneto98@gmail.com>
2022-11-15 03:13:34 -0300

    Remoção de cards desnecessários

--- lib/pages/project/project-edit.dart
+++ lib/pages/project/project-edit.dart
@@ -48,162 +48,10 @@ class _ProjectEditPageState extends State<ProjectEditPage> {
 
   @override
   Widget build(BuildContext context) {
-    final cardCriacaoProjeto = Card(
-      elevation: 4.0,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 5, 30, 20),
-        child: Form(
-          key: formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    labelText: "Nome",
-                    labelStyle: TextStyle(color: Colors.black)),
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.black, fontSize: 16.0),
-                controller: nomeController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Insira o nome do projeto";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              Padding(padding: EdgeInsets.only(top: 10.0)),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    labelText: "Cliente",
-                    labelStyle: TextStyle(color: Colors.black)),
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.black, fontSize: 16.0),
-                controller: clienteController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Insira o cliente do projeto";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              Padding(padding: EdgeInsets.only(top: 10.0)),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text("Data de início:",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontSize: 16.0)),
-                  ),
-                  Expanded(
-                    child: Text("${date.day}/${date.month}/${date.year}",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontSize: 18.0)),
-                  ),
-                  //const SizedBox(height: 16),
-                  ElevatedButton(
-                      onPressed: () async {
-                        DateTime? newDate = await showDatePicker(
-                          context: context,
-                          initialDate: date,
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2100),
-                        );
-
-                        if (newDate == null) {
-                          return;
-                        }
-
-                        setState(() => date = newDate);
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.purple),
-                      child: Icon(Icons.edit_calendar, color: Colors.white))
-                ],
-              ),
-              Padding(padding: EdgeInsets.only(top: 10.0)),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Prazo (em semanas)",
-                    labelStyle: TextStyle(color: Colors.black)),
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.black, fontSize: 16.0),
-                controller: prazoController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Insira o prazo do projeto";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              Padding(padding: EdgeInsets.only(top: 10.0)),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Duração do sprint (em dias)",
-                    labelStyle: TextStyle(color: Colors.black)),
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.black, fontSize: 16.0),
-                controller: duracaoSprintController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Insira a duração do sprint do projeto";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              Padding(padding: EdgeInsets.only(top: 10.0)),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Quantidade de times",
-                    labelStyle: TextStyle(color: Colors.black)),
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.black, fontSize: 16.0),
-                controller: quantidadeTimesController,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Insira a quantidade de times do projeto";
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 30, 15, 10),
-                  child: SizedBox(
-                      height: 50.0,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            _saveChanges();
-                          }
-                        },
-                        child: Text(
-                          "Salvar alterações",
-                          style: TextStyle(color: Colors.white, fontSize: 20.0),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.primaryPurple,
-                            textStyle: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.bold)),
-                      ))),
-            ],
-          ),
-        ),
-      ),
-    );
-
     return Scaffold(
         appBar: AppBar(
           backgroundColor: AppColors.white,
-          title: const Text("Editar dados do projeto",
+          title: const Text("Editar projeto",
               style: TextStyle(
                   color: Colors.black,
                   fontSize: 18,
@@ -217,7 +65,164 @@ class _ProjectEditPageState extends State<ProjectEditPage> {
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  cardCriacaoProjeto,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 5, 30, 20),
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                                labelText: "Nome",
+                                labelStyle: TextStyle(color: Colors.black)),
+                            textAlign: TextAlign.left,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0),
+                            controller: nomeController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Insira o nome do projeto";
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 10.0)),
+                          TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                                labelText: "Cliente",
+                                labelStyle: TextStyle(color: Colors.black)),
+                            textAlign: TextAlign.left,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0),
+                            controller: clienteController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Insira o cliente do projeto";
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 10.0)),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Text("Data de início:",
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(fontSize: 16.0)),
+                              ),
+                              Expanded(
+                                child: Text(
+                                    "${date.day}/${date.month}/${date.year}",
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(fontSize: 18.0)),
+                              ),
+                              ElevatedButton(
+                                  onPressed: () async {
+                                    DateTime? newDate = await showDatePicker(
+                                      context: context,
+                                      initialDate: date,
+                                      firstDate: DateTime(2000),
+                                      lastDate: DateTime(2100),
+                                    );
+
+                                    if (newDate == null) {
+                                      return;
+                                    }
+
+                                    setState(() => date = newDate);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.purple),
+                                  child: Icon(Icons.edit_calendar,
+                                      color: Colors.white))
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 10.0)),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: "Prazo (em semanas)",
+                                labelStyle: TextStyle(color: Colors.black)),
+                            textAlign: TextAlign.left,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0),
+                            controller: prazoController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Insira o prazo do projeto";
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 10.0)),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: "Duração do sprint (em dias)",
+                                labelStyle: TextStyle(color: Colors.black)),
+                            textAlign: TextAlign.left,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0),
+                            controller: duracaoSprintController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Insira a duração do sprint do projeto";
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 10.0)),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: "Quantidade de times",
+                                labelStyle: TextStyle(color: Colors.black)),
+                            textAlign: TextAlign.left,
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0),
+                            controller: quantidadeTimesController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Insira a quantidade de times do projeto";
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                          Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(20, 30, 15, 10),
+                              child: SizedBox(
+                                  height: 50.0,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      if (formKey.currentState!.validate()) {
+                                        _saveChanges();
+                                      }
+                                    },
+                                    child: Text(
+                                      "Salvar alterações",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20.0),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            AppColors.primaryPurple,
+                                        textStyle: TextStyle(
+                                            fontSize: 30,
+                                            fontWeight: FontWeight.bold)),
+                                  ))),
+                        ],
+                      ),
+                    ),
+                  ),
                   SizedBox(
                     height: 20,
                   ),
